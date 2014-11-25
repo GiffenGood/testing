d11683f26a8d90d8476b7703c0d32a207dc6fd01
mbeckius@gmail.com <mbeckius@gmail.com>
2014-11-24 23:08:42 -0500

    1st commit fb`

--- ConsoleApplication1/Program.cs
+++ ConsoleApplication1/Program.cs
@@ -11,6 +11,7 @@ namespace ConsoleApplication1
         static void Main(string[] args)
         {
             Console.WriteLine("yet some more stuff");
+            Console.WriteLine("I FB1 dawg!");
             Console.ReadLine();
         }
     }
